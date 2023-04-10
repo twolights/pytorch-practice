f75a5ae40c914b7be88c9f84aec8b720cad077fc
Yi-Kuang Chen <twolights@gmail.com>
2023-04-10 20:43:31 +0800

    Added training/validation/testing data loader

--- dogs-and-cats/datasets/__init__.py
+++ dogs-and-cats/datasets/__init__.py
@@ -1,1 +1,12 @@
 from .preprocess import image_to_tensor, CatsDogsDataset
+from torch.utils.data import random_split, DataLoader
+from .. import config
+
+
+all_training_set = CatsDogsDataset(config.TRAINING_SET_DIR, is_training=True)
+training_set, validation_set = random_split(all_training_set, [0.8, 0.2])
+testing_set = CatsDogsDataset(config.TESTING_SET_DIR, is_training=False)
+
+training_dataloader = DataLoader(training_set, batch_size=config.BATCH_SIZE, shuffle=True)
+validation_dataloader = DataLoader(training_set, batch_size=config.BATCH_SIZE, shuffle=True)
+testing_dataloader = DataLoader(training_set, batch_size=config.BATCH_SIZE, shuffle=True)
